0e642c5180639a92f740f37c24c6a3f97295f512
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-04-25 19:19:58 -0400

    Revert "Step 10."
    This reverts commit bc81d3b673a634ac2b4497e49643f4610b9a74b6.

--- Countdown/src/assets/dump.sql
+++ Countdown/src/assets/dump.sql
@@ -12,13 +12,13 @@ CREATE TABLE IF NOT EXISTS event(
 
 
 INSERT or IGNORE INTO event(
-	id, title, note, startDate, eventEnd, endDate, top, orders, counts) 
-	VALUES (1, 'Together', 'record we together days', '2011-11-25T23:59:59.999Z', 0, '', 1, 0, 0);
+	title, note, startDate, eventEnd, endDate, top, orders, counts) 
+	VALUES ('Together', 'record we together days', '2011-11-25T23:59:59.999Z', 0, '', 1, 0, null);
 
 INSERT or IGNORE INTO event(
-	id, title, note, startDate, eventEnd, endDate, top, orders, counts) 
-	VALUES (2, 'Australia Life', 'working & holiday', '2018-05-03T23:59:59.999Z', 1, '2019-01-03T23:59:59.999Z', 0, 1, 195);
+	title, note, startDate, eventEnd, endDate, top, orders, counts) 
+	VALUES ('Australia Life', 'working & holiday', '2018-05-03T23:59:59.999Z', 1, '2019-01-03T23:59:59.999Z', 0, 1, 195);
 	
 INSERT or IGNORE INTO event(
-	id, title, note, startDate, eventEnd, endDate, top, orders, counts) 
-	VALUES (3, 'Canada Life', 'working & holiday', '2019-03-21T23:59:59.999Z', 0, '', 0, 2, 0);
+	title, note, startDate, eventEnd, endDate, top, orders, counts) 
+	VALUES ('Canada Life', 'working & holiday', '2019-03-21T23:59:59.999Z', 0, '', 0, 2, null);
